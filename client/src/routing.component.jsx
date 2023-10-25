f0b4cda74ff95ee5065d3edb7595cf252ac1edfc
Yoann Godiet <yoann.3d@gmail.com>
2023-10-25 12:36:29 +0200

    adding the page management in routing

--- client/src/routing.component.jsx
+++ client/src/routing.component.jsx
@@ -1,6 +1,7 @@
 import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home/home.page";
 import Race from "./pages/race/race.page";
+import MembersManagement from "./pages/membersManagement/membersManagement.page";
 import Header from "./components/header/header.component";
 
 export default function Routing() {
@@ -25,6 +26,10 @@ export default function Routing() {
         {
           path: "/race",
           element: <Race />,
+        },
+        {
+          path: "/members",
+          element: <MembersManagement />,
         },
       ],
     },
