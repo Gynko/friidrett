3da2677444d95ed0ee6a4bcaa226e53f1fd4c1e0
shuseklepp <sandrahuseklepp@gmail.com>
2023-10-26 14:00:44 +0200

    Work with component

--- client/src/pages/race/race.page.jsx
+++ client/src/pages/race/race.page.jsx
@@ -1,4 +1,5 @@
-export default function Race(){
-return <p>Yay</p>
-};
+import Button from "../../components/button/button.component";
 
+export default function Race() {
+  return <Button>Knapp</Button>;
+}
