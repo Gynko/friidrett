ba6dea7c6c9abb7d469cc13fbc9714103d10b527
shuseklepp <sandrahuseklepp@gmail.com>
2023-10-27 13:55:24 +0200

    Changes to race.page buttons

--- client/src/pages/race/race.page.jsx
+++ client/src/pages/race/race.page.jsx
@@ -1,61 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Button from "../../components/button/button.component";
 import "./race.page.css";
+import SectionTitle from "../../components/sectionTitle/sectionTitle.component";
+import PageContainer from "../../components/pageContainer/pageContainer.component";
 
 function Race() {
-  const buttonNewRace = () => {
-    alert("New race");
-  };
-  const buttonEditRace = () => {
-    alert("Edit race");
-  };
-  const buttonDeleteRace = () => {
-    alert("Delete race");
-  };
-  const buttonFindRace = () => {
-    alert("Find race");
-  };
-
-  const buttonFullRaceList = () => {
-    alert("Full race list");
-  };
+  const raceButtons = [
+    { text: "New race", width: "button-small", path: "/new-race" },
+    { text: "Edit race", width: "button-small", path: "/edit-race" },
+    { text: "Delete race", width: "button-small", path: "/delete-race" },
+    { text: "Find race", width: "button-large", path: "/find-race" },
+    { text: "Full race list", width: "button-large", path: "/race-list" },
+  ];
 
   return (
-    <div className="page-container">
-      <Button
-        text="New race"
-        color="black"
-        width="button-small"
-        onClick={buttonNewRace}
-      />
+    <PageContainer>
+      <SectionTitle icon="races" titleTop="Races" titleBottom="management" />
 
-      <Button
-        text="Edit race"
-        color="black"
-        width="button-small"
-        onClick={buttonEditRace}
-      />
-
-      <Button
-        text="Delete race"
-        color="black"
-        width="button-small"
-        onClick={buttonDeleteRace}
-      />
-      <Button
-        text="Find race"
-        color="black"
-        width="button-large"
-        onClick={buttonFindRace}
-      />
-
-      <Button
-        text="Full race list"
-        color="black"
-        width="button-large"
-        onClick={buttonFullRaceList}
-      />
-    </div>
+      {raceButtons.map((button, index) => (
+        <Link key={index} to={button.path}>
+          <Button text={button.text} color="black" width={button.width} />
+        </Link>
+      ))}
+    </PageContainer>
   );
 }
 
