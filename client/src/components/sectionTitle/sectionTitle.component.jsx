675022d74d932f69a52a2d8df4e1a78e9ae69ea8
yardokun <yardenna.okun@gmail.com>
2023-10-27 12:50:36 +0200

    Finished sectionTitle comp and added pageContainer comp

--- client/src/components/sectionTitle/sectionTitle.component.jsx
+++ client/src/components/sectionTitle/sectionTitle.component.jsx
@@ -1,14 +1,23 @@
+import "./sectionTitle.styles.css";
 import members from "../../assets/graphics/members.png";
 import races from "../../assets/graphics/races.png";
 import results from "../../assets/graphics/results.png";
 
-export default function SectionTitle({ membersIcon }) {
+export default function SectionTitle({ icon, titleTop, titleBottom }) {
+  function imageChanger() {
+    if (icon === "members") return members;
+    else if (icon === "races") return races;
+    else if (icon === "results") return results;
+  }
+
   return (
-    <div>
-      <img src={membersIcon} height="64px" width="64px"></img>
-      <img src={members} height="64px" width="64px"></img>
-      <img src={races} height="64px" width="64px"></img>
-      <img src={results} height="64px" width="64px"></img>
+    <div className="section-title-container">
+      <img src={imageChanger()} height="70px" width="64px"></img>
+      <div className="section-title-wrapper">
+        <h1 className="section-title-text">
+          {titleTop} {"\n"} {titleBottom}
+        </h1>
+      </div>
     </div>
   );
 }
