20b2219f8a7eedbc57bc42b64e080f842906b245
shuseklepp <sandrahuseklepp@gmail.com>
2023-10-30 10:08:46 +0100

    Imported in race-pages

---
+++ client/src/pages/race/raceNew/raceNew.page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "../../../components/button/button.component"
+import "./raceNew.styles.css"
+import SectionTitle from "../../../components/sectionTitle/sectionTitle.component";
+import PageContainer from "../../../components/pageContainer/pageContainer.component";
+
+
+function raceNew() {
+
+
+return(
+    <PageContainer>
+    <SectionTitle icon="races" titleTop="Races" titleBottom="management" />
+
+      <Link key={index} to={button.path}>
+        <Button text={button.text} color="black" width={button.width} />
+      </Link>
+    
+  </PageContainer>
+)
+
+
+}
+
