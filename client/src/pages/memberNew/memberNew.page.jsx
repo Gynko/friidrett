5711995f16c26a3bb736f85bf17a0468f67eb71a
yardokun <yardenna.okun@gmail.com>
2023-10-30 11:42:17 +0100

    Started making the inputDate comp

--- client/src/pages/memberNew/memberNew.page.jsx
+++ client/src/pages/memberNew/memberNew.page.jsx
@@ -1,6 +1,7 @@
 import SectionTitle from "../../components/sectionTitle/sectionTitle.component";
 import PageContainer from "../../components/pageContainer/pageContainer.component";
 import Heading from "../../components/heading/heading.component";
+import inputDate from "../../components/inputDate/inputDate.component";
 
 export default function MemberNew() {
   return (
@@ -12,6 +13,7 @@ export default function MemberNew() {
           titleBottom="management"
         />
         <Heading text="New member" />
+        <inputDate />
       </PageContainer>
     </main>
   );
