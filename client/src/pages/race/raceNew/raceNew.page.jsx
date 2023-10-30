52bd267383c169108eb654e5d88ff16f5c34ef8b
shuseklepp <sandrahuseklepp@gmail.com>
2023-10-30 10:43:39 +0100

    Button functions i race.New

--- client/src/pages/race/raceNew/raceNew.page.jsx
+++ client/src/pages/race/raceNew/raceNew.page.jsx
@@ -6,14 +6,22 @@ import PageContainer from "../../../components/pageContainer/pageContainer.compo
 
 
 function raceNew() {
-
+    const buttonProps = {
+        text: "Add members",
+        width: "button-small",
+        path: "/new-race" 
+    }
 
 return(
     <PageContainer>
     <SectionTitle icon="races" titleTop="Races" titleBottom="management" />
 
       <Link key={index} to={button.path}>
-        <Button text={button.text} color="black" width={button.width} />
+        <Button text={button.text} color="red" width={button.width} />
+      </Link>
+
+      <Link key={index} to={button.path}>
+        <Button text={button.text} color="yellow" width={button.width} />
       </Link>
     
   </PageContainer>
