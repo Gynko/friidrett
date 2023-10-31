bcc3734f3b6a34c06cbf5b458f55533a9faead32
shuseklepp <sandrahuseklepp@gmail.com>
2023-10-31 13:31:53 +0100

    Working with input components and race.new

--- client/src/pages/race/raceNew/raceNew.page.jsx
+++ client/src/pages/race/raceNew/raceNew.page.jsx
@@ -4,56 +4,61 @@ import Button from "../../../components/button/button.component";
 import SectionTitle from "../../../components/sectionTitle/sectionTitle.component";
 import PageContainer from "../../../components/pageContainer/pageContainer.component";
 import InputText from "../../../components/inputText/inputText.component";
-import Heading from "../../../components/heading/heading.component"
+import Heading from "../../../components/heading/heading.component";
 
 
-function NewRace() {
-    const [formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
-        email: '',
-        
-    });
+export default function NewRace() {
+  const today = new Date().toISOString().split("T")[0];
+  const [formData, setFormData] = useState({
+    raceDate: "",
+    distance: "",
+  });
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
-    return (
-        <PageContainer>
-            <SectionTitle icon="races" titleTop="Races" titleBottom="management" />
+  return (
+    <PageContainer>
+      <SectionTitle icon="races" titleTop="Races" titleBottom="management" />
 
-            <Heading text="New Race" />
+      <Heading text="New Race" />
+      <form onSubmit={handleSubmit}>
+       
+      <input 
+      type="date"
+      name="raceDate"
+      value={formData.raceDate}
+      onChange={handleInputChange} />
 
-            <InputText 
-                placeholder="First Name"
-                value={formData.firstName}
-                onChange={handleInputChange}
-                name="firstName"
-            />
+        <InputText
+          placeholder="distance (km)"
+          type="number"
+          value={formData.distance}
+          onChange={handleInputChange}
+          name="distance"
+          min="0"
+          step="1.00"
+        />
 
-            <InputText 
-                placeholder="Last Name"
-                value={formData.lastName}
-                onChange={handleInputChange}
-                name="lastName"
-            />
+        {/* Må legge til rød knapp med modal */}
+        {/*    <Button text="Add member" color="red" width="button-large" /> */}
 
-            <InputText 
-                placeholder="Email"
-                value={formData.email}
-                onChange={handleInputChange}
-                name="email"
-                type="email"
-            />
-
-<Button text="Edit member" color="yellow" width="button-large" />
-  </PageContainer>
-    );
+        <Button
+          text="Submit new race"
+          color="yellow"
+          width="button-large"
+          type="submit"
+        />
+      </form>
+    </PageContainer>
+  );
 }
-
-export default NewRace;
